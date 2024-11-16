5443f575c974ca9b2796b75468fdfbe9c4a35a93
chiro <80062293+Chiro29@users.noreply.github.com>
2024-11-16 01:08:34 +0100

    feat(project): added caesar cipher function

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -9,13 +9,24 @@ def checkNumber(n)
   n =  n.to_i
 end
 
+def caesar_cipher(s, n)
+  array = ("a".."z").to_a
+  s.gsub(/[a-z]/) do |letter|
+    index = (array.index(letter) + n) % array.size
+    array[index]
+  end
+end
+
 puts "Write something"
 string = gets.chomp
 
-until string =~ /^[a-zA-Z]+$/ do
-  puts "Only letters of the alphabelt"
+until string =~ /^[a-zA-Z\s]+$/ do
+  puts "Only letters of the alphabelt and space"
   string = gets.chomp
 end
+
+string = string.downcase
+puts "The phrase you written: #{string}"
 
 puts "Choose a number between 1 to 25"
 key = gets.chomp
@@ -27,3 +38,9 @@ until key.between?(1, 25) do
   key = gets.chomp
   key = checkNumber(key)
 end
+
+puts "The number you chose: #{key}"
+
+new_string = caesar_cipher(string, key)
+
+puts "Original: #{string}\nencrypted: #{new_string}"
